b091b0cd0901b7b35a6d890e96ebe0a655b8718c
asingla <anilsingla111@gmail.com>
2025-03-25 14:45:40 +0530

    cleaning the repo

--- discussion.md
+++
@@ -1,6 +0,0 @@
-## Community Meetup Discussion Notes
-
----
-25 Mar 2025
----
-Initial Checkin
--- src/assets/theme/functions/linearGradient.tsx
+++
@@ -1,6 +0,0 @@
-
-function linearGradient(color: any, colorState: any, angle = 195) {
-  return `linear-gradient(${angle}deg, ${color}, ${colorState})`;
-}
-
-export default linearGradient;
