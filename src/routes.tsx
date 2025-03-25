c3ea7026deffc38145485d3b8070026c47f630b9
ANIL SINGLA <anilsingla111@gmail.com>
2025-03-25 15:30:08 +0530

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 // @mui icons
 import GitHubIcon from "@mui/icons-material/GitHub";
-import Icon from "@mui/material/Icon";
+//import Icon from "@mui/material/Icon";
 //import ModelOfInterest from "./pages/ModelOfInterest";
 //import VulnerabilityOfInterest from "./pages/VulnerabilityOfInterest";
 //import ProjectOfInterest from "./pages/ProjectOfInterest";
